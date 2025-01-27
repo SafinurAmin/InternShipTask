8575cade636b2e33dbde3bb57ab97c67d1ea846b
Safinur Amin <135307358+SafinurAmin@users.noreply.github.com>
2025-01-27 19:46:13 +0600

    Add files via upload

---
+++ pages/page.js
@@ -0,0 +1,21 @@
+'use client'
+import Hero from '../components/Hero';
+import Profile from '../components/Profile';
+import Projects from '../components/Projects';
+import Testimonial from '../components/Testimonial';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+
+export default function Home() {
+  return (
+    <div className="text-white" style={{ background: 'var(--background)' }}>
+      <Header />
+      <Hero />
+      <Profile />
+      <Projects />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+};
